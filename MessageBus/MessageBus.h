e2f3aeff9f1bf1cfb30f1bf5944b85e80bb79ec0
Martin Kloeppner <mkloeppner@me.com>
2016-06-30 23:34:43 +0200

    Initial Commit

---
+++ MessageBus/MessageBus.h
@@ -0,0 +1,19 @@
+//
+//  MessageBus.h
+//  MessageBus
+//
+//  Created by Martin Klöppner on 30.06.16.
+//  Copyright © 2016 Martin Klöppner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MessageBus.
+FOUNDATION_EXPORT double MessageBusVersionNumber;
+
+//! Project version string for MessageBus.
+FOUNDATION_EXPORT const unsigned char MessageBusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MessageBus/PublicHeader.h>
+
+
